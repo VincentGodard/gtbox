467906c69e85e68e57519c0c3e9ec380394168d4
Vincent Godard <godard@cerege.fr>
2021-12-10 21:40:52 +0100

    changes to test_terra.R

--- dev/tests/test_terra.R
+++ dev/tests/test_terra.R
@@ -11,6 +11,34 @@ dem = rast(dem_cevennes)
 rm(dem_cevennes)
 writeRaster(dem,"/tmp/dem.tif")
 
+# compute stack and network (,precip=precip) ----
+#precip = dem/dem
+st = process_dem(dem,th_px,gisbase=gisbase,to_net=FALSE)
+writeRaster(st$st_id,"/tmp/streams.tif",overwrite=T)
+p1 = c(556581.660,4893704.291)
+p2 = c(556905.252,4884893.481)
+p3 = c(571818.787,4877159.262)
+out = rbind(p1,p2,p3)
+out1 = terra::vect(out,type="points",crs=crs(dem))
+out1$a=0
+bas1 = get_basins(st,out1,gisbase)
+
+w=200
+acc = st$acc
+df_out = cbind(terra::as.data.frame(out1),geom(out1)[,c("x","y")])
+for (i in 1:nrow(df_out)){
+buf = terra::buffer(out1[i,],w)
+tmp = crop(acc,buf)
+tmp = terra::as.data.frame(tmp,xy=T)
+df_out$x_new[i] = tmp[which.max(tmp$acc),]$x
+df_out$y_new[i] = tmp[which.max(tmp$acc),]$y
+}
+out_new = vect(as.matrix(df_out[,c("x_new","y_new")]),atts=df_out,crs=crs(out1))
+
+bas2 = get_basins(st,out_new,gisbase)
+plot(bas2)
+
+
 # Swath profiles ----
 x1 = 583695
 y1 = 4875921
@@ -35,9 +63,20 @@ data = swath_profile(dem,x1,y1,x2,y2,w_buf,100) # computation of the swath profi
 C0 =  compute_curvature(dem,win=5,gisbase)
 S0 =  compute_slope(dem,win=5,gisbase)
 
-# compute stack and network (,precip=precip) ----
-#precip = dem/dem
-st = process_dem(dem,th_px,gisbase=gisbase,to_net=FALSE)
+
+# test types ----
+stream1 <- st$st_id
+mat1 = as.matrix(stream1,wide=T)
+object.size(mat1)/1e6
+rgrass7::execGRASS("r.mapcalc", expression="streams0 = round(streams)")
+stream2 = terra::rast(raster::raster(rgrass7::readRAST(c("streams0"))))
+mat2 = as.matrix(stream2,wide=T)
+object.size(mat2)/1e6
+
+
+
+
+
 
 net = get_network(st,gisbase,clip=TRUE)
 
